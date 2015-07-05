2ab5826a5601ec4d70c7d2d62d29624409e74d9e
Brendan Graetz <github@bguiz.com>
2015-07-05 15:03:18 +1000

    =BG= connect up data set to range slider
    - pointing to a by-state data set
    - pointing to a state boundaries geojson file

--- client/control.js
+++ client/control.js
@@ -7,7 +7,7 @@ function setUpListeners() {
     .forEach(function eachDataSetId(dataSetId) {
       var elem = document.getElementById(dataSetId);
       elem.addEventListener('click', function onSelectDataSet() {
-        loadDataSet(dataSetId);
+        loadDataSet(dataSetId, displayDataSet);
       }, false);
     });
 
@@ -15,38 +15,61 @@ function setUpListeners() {
     .forEach(function eachDataSetWithRangeSliderId(dataSetId) {
       var elem = document.getElementById(dataSetId);
 
-      elem.addEventListener('input', function onRangeInput() {
-          this.setAttribute('value', this.value);
-        console.log('onRangeInput', this.value);
-      }, false);
+      loadDataSet(dataSetId, function onDone(err, results) {
+
+        elem.addEventListener('input', function onRangeInput() {
+          var rangeValue = this.value;
+          this.setAttribute('value', rangeValue);
+          console.log('onRangeInput', rangeValue);
+          var dataSet = results[''+rangeValue];
+          var err;
+          if (!dataSet) {
+            err = 'No data set found for '+rangeValue;
+          }
+          displayDataSet(err, dataSet);
+        }, false);
+      });
+
+
     });
 }
 
-function loadDataSet(dataSetId) {
+function loadDataSet(dataSetId, onDone) {
   console.log('loadDataSet', dataSetId);
   var url = 'data/'+dataSetId+'.json';
   xhrGet(url, function onGot(err, results) {
-    if (err) {
-      console.error(err);
-      return;
+    if (!err) {
+      try {
+        results = JSON.parse(results);
+        console.log('got', url, results);
+      }
+      catch (ex) {
+        err = ex;
+      }
     }
-    results = JSON.parse(results);
-    console.log('got', url, results);
 
-    displayDataSet(results);
+    onDone(err, results);
   });
 }
 
-function displayDataSet(results) {
+function displayDataSet(err, results) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('displayDataSet', results);
+
   // Merge the data result into the geojson
   geoJsonResult.features.map(function eachFeature(feature) {
     var id = feature.properties.id;
+    var name = feature.properties.name;
     var result = results[id];
     if (!!result) {
       // Override all properties,
-      // except for ID, which we must guarantee stay unmodified
+      // except for `id` and `name`, which we must guarantee stay unmodified
       feature.properties = result;
       feature.properties.id = id;
+      feature.properties.name = name;
     }
     return feature;
   });
@@ -66,10 +89,12 @@ function displayDataSet(results) {
     getPropertyColourDomain: getPropertyColourDomain,
   });
   function getPropertyValue(props) {
-    return props.MedianHousePrice;
+    return props.schools;
+    // return props.MedianHousePrice;
   }
   function getPropertyDisplayName(props) {
-    return 'Median House Price';
+    return 'New Schools Needed';
+    // return 'Median House Price';
   }
   function getPropertyColour(props) {
     return colourScale(getPropertyValue(props))
